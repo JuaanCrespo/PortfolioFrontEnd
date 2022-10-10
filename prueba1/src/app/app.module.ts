75212ec85f4758898452738e9c390d40ae485687
Juan <juancres1@gmail.com>
2022-10-10 17:06:06 -0300

    nuevas pruebas

--- prueba1/src/app/app.module.ts
+++ prueba1/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NuevoComponent } from './components/nuevo/nuevo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NuevoComponent
   ],
   imports: [
     BrowserModule,
