7988881676940036a2bf2614e86bd5239e5e4fa5
Juan <juancres1@gmail.com>
2022-11-02 01:56:54 -0300

    Mi lista de tareas terminada

--- pruebaAngular2/src/app/components/tasks-item/tasks-item.component.ts
+++ pruebaAngular2/src/app/components/tasks-item/tasks-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Task } from '../../Task';
 import { TASKS } from '../../mock-tasks';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
@@ -6,14 +6,21 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-tasks-item',
   templateUrl: './tasks-item.component.html',
-  styleUrls: ['./tasks-item.component.css']
+  styleUrls: ['./tasks-item.component.css'],
 })
 export class TasksItemComponent implements OnInit {
-@Input() task: Task = TASKS[0];
-faTimes = faTimes;
-  constructor() { }
+  @Input() task: Task = TASKS[0];
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter();
+  faTimes = faTimes;
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  onDelete(task: Task) {
+    this.onDeleteTask.emit(task);
   }
 
+  onToggle(task: Task) {
+    this.onToggleReminder.emit(task);
+  }
 }
